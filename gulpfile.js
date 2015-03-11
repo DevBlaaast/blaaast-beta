dc78c28fd8f8b2ae729e903a49532d803467db29
Tom Coquereau <tom@thau.me>
2015-03-11 09:44:30 +0100

    Add compression

--- gulpfile.js
+++ gulpfile.js
@@ -4,64 +4,74 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     jshint = require('gulp-jshint'),
     notify = require('gulp-notify'),
-    concat = require('gulp-concat');
-    // uncss = require('gulp-uncss'),
-    // zopfli = require("gulp-zopfli"),
-    // connect = require('gulp-connect'),
-    // minifyhtml = require('gulp-minify-html'),
-    // inlinesource = require('gulp-inline-source');
+    concat = require('gulp-concat'),
+    plumber = require('gulp-plumber'),
+    uncss = require('gulp-uncss'),
+    zopfli = require("gulp-zopfli"),
+    connect = require('gulp-connect'),
+    minifyhtml = require('gulp-minify-html'),
+    inlinesource = require('gulp-inline-source');
+
+gulp.task('connect', function() {
+  connect.server({
+    port: 3000,
+    livereload: true
+  });
+});
 
 gulp.task('css', function() {
     return gulp.src('./scss/main.scss')
+        .pipe(plumber())
         .pipe( sass() )
         .pipe( gulp.dest('./build/css') )
-        // .pipe( connect.reload() )
-        // .pipe( notify('CSS task complete!') )
+        .pipe( connect.reload() )
+        .pipe( notify('CSS task complete!') )
 });
 
 gulp.task('js', function() {
     return gulp.src('./js/**/*.js')
+        .pipe(plumber())
         .pipe( concat('all.js') )
         .pipe( jshint() )
         .pipe( jshint.reporter('default') )
-        .pipe( gulp.dest('./build/js/') );
-        // .pipe( connect.reload() )
-        // .pipe( notify('JS task complete!') )
+        .pipe( gulp.dest('./build/js/') )
+        .pipe( connect.reload() )
+        .pipe( notify('JS task complete!') )
 });
 
-// gulp.task('html', function() {
-//     return gulp.src('./src/*.html')
-//         .pipe( connect.reload() )
-//         .pipe( gulp.dest('./build') )
-// });
+gulp.task('html', function() {
+    return gulp.src('./*.html')
+        .pipe( connect.reload() )
+});
 
+gulp.task('uncss', function() {
+    return gulp.src('./build/css/*.css')
+        .pipe( uncss({ html: ['./build/*.html'] }) )
+        .pipe( gulp.dest('./build/css') )
+});
 
-// gulp.task('uncss', function() {
-//     return gulp.src('./build/css/*.css')
-//         .pipe( uncss({ html: ['./build/*.html'] }) )
-//         .pipe( gulp.dest('./build/css') )
-// });
+gulp.task('minify', function() {
+    return gulp.src('./*.html')
+        .pipe( inlinesource( { compress: true } ) )
+        .pipe( minifyhtml() )
+        .pipe( gulp.dest('./build') )
+});
 
-// gulp.task('minify', function() {
-//     return gulp.src('./build/*.html')
-//         .pipe( inlinesource( {compress: true} ) )
-//         .pipe( minifyhtml() )
-//         .pipe( gulp.dest('./public') )
-// });
-
-// gulp.task('compress', function() {
-//     return gulp.src('./public/*.html')
-//         .pipe( zopfli() )
-//         .pipe( gulp.dest('./public') )
-// });
+gulp.task('compress', function() {
+    return gulp.src('./*.html')
+        .pipe( inlinesource( { compress: true } ) )
+        .pipe( zopfli() )
+        .pipe( minifyhtml() )
+        .pipe( gulp.dest('./') )
+});
 
 /* Default task */
-gulp.task('default', [/*'connectBuild',*/ 'watch'], function() {
+gulp.task('default', ['connect', 'watch'], function() {
     gulp.start('css', 'js');
 });
 
 /* Deploy task */
-gulp.task('deploy', ['connectPublic'], function() {
+gulp.task('deploy', ['connect', 'css', 'js'], function() {
     gulp.start('uncss', 'minify', 'compress');
 });
 
@@ -69,5 +79,5 @@ gulp.task('deploy', ['connectPublic'], function() {
 gulp.task('watch', function() {
     gulp.watch('./scss/**/*.scss', ['css']);
     gulp.watch('./js/**/*.js', ['js']);
-    // gulp.watch('./*.html', ['html']);
+    gulp.watch('./*.html', ['html']);
 });
