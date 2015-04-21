fc83ddc05cb269862bf981afabe389b789fc22be
Tom Coquereau <tom@thau.me>
2015-04-21 11:33:08 +0200

    Add smooth scrolling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import Modal from './modal';
+import SmoothScroll from './smooth-scroll';
 
 new Modal();
+new SmoothScroll();
---
+++ js/smooth-scroll.js
@@ -0,0 +1,68 @@
+// Polyfills:
+// - classList
+
+export default class Modal {
+
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    let navLinks = document.getElementsByClassName('js-smooth-link');
+
+    [].forEach.call(navLinks, (el) => {
+      el.addEventListener('click', (ev) => {
+        ev.preventDefault();
+        this.smoothScroll(ev.target.href.split('#')[1]);
+      }, false);
+    });
+  }
+
+  currentYPosition() {
+    // Firefox, Chrome, Opera, Safari
+    if (self.pageYOffset) return self.pageYOffset;
+    // Internet Explorer 6 - standards mode
+    if (document.documentElement && document.documentElement.scrollTop)
+      return document.documentElement.scrollTop;
+    // Internet Explorer 6, 7 and 8
+    if (document.body.scrollTop) return document.body.scrollTop;
+    return 0;
+  }
+
+  elmYPosition(eID) {
+    console.log('eID: ', eID);
+    let elm = document.getElementById(eID);
+    let y = elm.offsetTop;
+    let node = elm;
+    while (node.offsetParent && node.offsetParent != document.body) {
+      node = node.offsetParent;
+      y += node.offsetTop;
+    } return y;
+  }
+
+  smoothScroll(eID) {
+    let startY = this.currentYPosition();
+    let stopY = this.elmYPosition(eID);
+    let distance = stopY > startY ? stopY - startY : startY - stopY;
+    if (distance < 100) {
+      scrollTo(0, stopY); return;
+    }
+    let speed = Math.round(distance / 100);
+    if (speed >= 20) speed = 20;
+    let step = Math.round(distance / 25);
+    let leapY = stopY > startY ? startY + step : startY - step;
+    let timer = 0;
+    if (stopY > startY) {
+      for ( let i=startY; i<stopY; i+=step ) {
+        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+        leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+      } return;
+    }
+    for ( let i=startY; i>stopY; i-=step ) {
+      setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+      leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+    }
+    return false;
+  }
+
+}
